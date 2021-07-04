2e88df692227be63288ca01c2973831c98035b6f
mounir beggas <beggas.sl@gmail.com>
2021-07-04 11:02:29 +0100

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.Socket;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -12,19 +10,15 @@ public class Main {
             try {
                 var client = HttpClient.newHttpClient();
                 HttpRequest request = HttpRequest.newBuilder()
-                        .uri(URI.create("http://41.226.32.58:8096/setip"))
+                        .uri(URI.create("http://41.226.32.58:8096/setitharip"))
                         .build();
                 var response = client.send(request, HttpResponse.BodyHandlers.ofString());
                 System.out.println(response.body());
                 Thread.sleep(300000);
             } catch (Exception e) {
                 System.out.println(e);
+                Thread.sleep(300000);
             }
         } while (true);
-
-        // create a client
-
     }
-
-
 }
